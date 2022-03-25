0c51de967adc31c044d5837ea7f958e6fddbfac1
Eugene Nikolsky <e@egeek.me>
2022-03-25 17:05:10 +0200

    support parsing objects

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,24 @@
 module Lib where
 
 import Data.Aeson
+import qualified Data.Aeson.Key as AK
+import qualified Data.Aeson.KeyMap as AKM
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 
 -- | Key path to the value, e.g. @["solar_system", "planet", "earth"]@.
 type Path = [String]
+
 newtype JKey = JKey { unJKey :: Path }
-  deriving (Show)
+  deriving (Eq, Ord)
+
+instance Show JKey where
+  show (JKey l) = show l
+
 -- TODO a value can also be a list of strings now.
 newtype JValue = JValue { unJValue :: T.Text }
-  deriving (Show)
+instance Show JValue where
+  show (JValue l) = show l
 
 -- | Representation of a JSON translation file.
 type JKeyValues = M.Map JKey JValue
@@ -36,5 +44,8 @@ values :: Value -> JKeyValues
 values = go []
   where
     go :: Path -> Value -> JKeyValues
-    go keyPath (String s) = M.singleton (JKey keyPath) (JValue s)
-    -- go keys (Object hashMap) =
+    go keyPath (String s) = M.singleton (JKey . reverse $ keyPath) (JValue s)
+    go keyPath (Object hashMap) = AKM.foldMapWithKey folder hashMap
+      where
+        folder :: Key -> Value -> JKeyValues
+        folder key = go (AK.toString key : keyPath)
