3964d785e01d36ffed43f3f21526388a3aadc453
Eugene Nikolsky <e@egeek.me>
2022-03-30 16:04:16 +0300

    Calculate and print diff of two jsons

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import Data.Aeson
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Lazy.Char8 as BSL8
+import Data.Map.Strict ((\\))
+import Lib
+import System.Environment (getArgs)
+import System.Exit (die)
+
 main :: IO ()
-main = pure ()
+main = do
+  args <- getArgs
+  case args of
+    [file1, file2] -> printDiff file1 file2
+    _ -> die "Usage: jsondiff <file1> <file2>"
+
+printDiff :: FilePath -> FilePath -> IO ()
+printDiff file1 file2 = do
+   (Just value1 :: Maybe JKeyValues) <- fmap values . decode <$> BSL.readFile file1
+   (Just value2 :: Maybe JKeyValues) <- fmap values . decode <$> BSL.readFile file2
+   let diff = value1 \\ value2
+   BSL8.putStrLn . encode . unValues $ diff
