10449c7adbb3ef4ca85dc84ac47deb0722cf9995
Eugene Nikolsky <e@egeek.me>
2022-06-03 20:40:51 +0300

    Inline test json files into `LibSpec.hs`
    Since those files were small, it makes sense to move them into the
    source, which is much easier with the raw quasi-quoter. This also fixes
    a warning when building the project:
    
    ```
    package.yaml: Ignoring unrecognized field $.tests.jsondiff-test.extra-source-files
    ```

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,11 +1,11 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module LibSpec where
 
 import Data.Aeson
-import System.FilePath
 import Test.Hspec
+import Text.RawString.QQ (r)
 import qualified Data.ByteString.Lazy as BL
 
 import Lib
@@ -15,22 +15,68 @@ spec = describe "diff" $ parallel $ do
   context "when translation file is empty" $
     it "returns the english file" $ do
       let empty = "{}"
-      english <- BL.readFile $ takeDirectory __FILE__ </> "files/en.json"
       let (Just actual) = decode $ diff english empty
           (Just expected) = decode english :: Maybe Value
       actual `shouldBe` expected
 
   context "when translation file is also english" $
     it "returns empty output" $ do
-      english <- BL.readFile $ takeDirectory __FILE__ </> "files/en.json"
       let (Just actual) = decode $ diff english english
           (Just expected) = decode "{}" :: Maybe Value
       actual `shouldBe` expected
 
   context "when translation file is not empty" $
     it "returns the keys missing from the translation file" $ do
-      english <- BL.readFile $ takeDirectory __FILE__ </> "files/en.json"
-      translation <- BL.readFile $ takeDirectory __FILE__ </> "files/translation.json"
       let (Just actual) = decode $ diff english translation
-      (Just expected) <- decodeFileStrict $ takeDirectory __FILE__ </> "files/diff.json" :: IO (Maybe Value)
+          (Just expected) = decode expectedDiff :: Maybe Value
       actual `shouldBe` expected
+
+english :: BL.ByteString
+english = [r|
+{
+  "main": {
+    "no": "no",
+    "save": "Save",
+    "yes": "yes"
+  },
+  "group": {
+    "inside": {
+      "group": {
+        "cancel": "Cancel"
+      }
+    },
+    "remove": "remove"
+  },
+  "root": "Root"
+}
+|]
+
+translation :: BL.ByteString
+translation = [r|
+{
+  "main": {
+    "save": "1234"
+  },
+  "group": {
+    "remove": "abcd"
+  }
+}
+|]
+
+expectedDiff :: BL.ByteString
+expectedDiff = [r|
+{
+  "main": {
+    "no": "no",
+    "yes": "yes"
+  },
+  "group": {
+    "inside": {
+      "group": {
+        "cancel": "Cancel"
+      }
+    }
+  },
+  "root": "Root"
+}
+|]
